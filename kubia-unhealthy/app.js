e16605fe8099612b981500c4f8b8021c50839630
knrt10 <tripathi.kautilya@gmail.com>
2019-12-10 00:55:10 +0530

    Added more on liveness probe Signed-off-by: knrt10 <tripathi.kautilya@gmail.com>

--- kubia-unhealthy/app.js
+++ kubia-unhealthy/app.js
@@ -1,10 +1,21 @@
 const http = require('http');
 const os = require('os');
+
 console.log("Kubia server starting...");
-let handler = function (request, response) {
+
+let requestCount = 0;
+
+let handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
+  requestCount++;
+  if (requestCount > 5) {
+    response.writeHead(500);
+    response.end("I'm not well. Please restart me!");
+    return;
+  }
   response.writeHead(200);
   response.end("You've hit " + os.hostname() + "\n");
 };
+
 let www = http.createServer(handler);
 www.listen(8080);
